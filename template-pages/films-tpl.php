4c20d71e4140317141ff41cfba448e3eedc4c9a9
Romron <romron@ukr.net>
2020-10-07 15:00:47 +0300

    Все блоки в films-tpl.php отображаются корректно адаптивности НЕТу

--- template-pages/films-tpl.php
+++ template-pages/films-tpl.php
@@ -51,7 +51,7 @@
 </article><!-- #post-## -->
 
 
-
+<?php get_footer(); ?>
 
 
 <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script><?php get_header(); ?>
