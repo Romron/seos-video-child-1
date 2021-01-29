ed290ca5a7e7e4bdeef5afe8f068ef23832cce3e
romron@ukr.net <romron@ukr.net>
2021-01-29 16:22:04 +0200

    Простое меню готово. Нужно ли декорировать?

--- sidebar-left_sidebar.php
+++ sidebar-left_sidebar.php
@@ -6,11 +6,6 @@
   <?php dynamic_sidebar( 'left_sidebar' ); ?>
   <?php endif; ?>
 
-
-<div id="filters">
-	<h3>Подбор фильмов</h3>
-
-
 	<?
 		$args = array(
 			'public'   => true,
@@ -20,50 +15,43 @@
 		$list_taxonomys = get_taxonomies($args,$output);
 	?>
 
-
+<div id="filters">
 	<nav class="menu-films">
-		<ul>
+		<div class="menu-films__title">
+			<h3>Подбор фильмов</h3>
+		</div>
 			<!-- Меню подбора фильмов -->
-				   <!-- -----  wp_nav_menu ------ -->
-					<?php  
-						// if (has_nav_menu('menu_left_sibear_top')) {
-						// 	wp_nav_menu([
-						// 		// 'menu' => 'Первое меню',
-						// 		'theme_location' => 'menu_left_sibear_top',
-						// 	]);
-						// }
-					?>
-				   <!-- -----  wp_nav_menu ------ -->
-
-			<? foreach ($list_taxonomys as $taxonomy) { ?>
-					<ul>	<h4><?  echo($taxonomy->label)?></h4>
-						<? $arr_terms = get_terms( $taxonomy->name, array("hide_empty" => false));
-						foreach ($arr_terms as $obj_term) {
-							$link_to_page_terms = get_term_link($obj_term->slug,$obj_term->taxonomy);
-						?>	
-							<li><a href="<?echo($link_to_page_terms)?>"><?echo($obj_term->slug)?></a></li>
-						<? } ?>
-						</ul>
+			<? foreach ($list_taxonomys as $taxonomy) { 
+					if (($taxonomy->label == 'Актёры') || ($taxonomy->label == 'Режиссеры') || ($taxonomy->label == 'Сценаристы') || ($taxonomy->label == 'Директора')){ 
+						continue;}
+			?>
+					<ul class="ul-first-level">	
+						<li class="ul-first-level__li-first-level">
+							<h4 class="ul-first-level__li-first-level__h4">
+								<?  echo($taxonomy->label)?>
+							</h4>
+							<? 
+								$arr_terms = get_terms( $taxonomy->name, array("hide_empty" => false));
+								foreach ($arr_terms as $obj_term) {
+									$link_to_page_terms = get_term_link($obj_term->slug,$obj_term->taxonomy);
+							?>	
+							<ul class="ul-second-level">	 						
+								<li class="ul-second-level__li-second-level">
+									<a href="<?echo($link_to_page_terms)?>"><?echo($obj_term->name)?></a>
+								</li>
+							</ul>
+							<? } ?>
+						</li>
+					</ul>
 			<?	} ?>
 			
 
 
 
 
-
-
-
-
-
-
-
-
-		</ul>
-
 	</nav>
-
-
 </div>
+
 <div id="advertising_space_left">
 	<h3>advertising_space</h3>
 
